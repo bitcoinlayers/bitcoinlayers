f4322b357523380eec45779e9a3c357bd80d3ce6
Red <bradley.t.sheehan@gmail.com>
2024-04-11 07:14:26 -0600

    fix layer page

--- app/layers/[slug]/page.tsx
+++ app/layers/[slug]/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 import React from "react";
 import { notFound } from "next/navigation";
-import InfrastructureProps from "@/components/infrastructure/infrastructureProps";
-// import InfrastructureHead from "@/components/infrastructure/infrastructureHead";
-// import InfrastructureSummary from "@/components/infrastructure/infrastructureSummary";
-// import InfrastructureChart from "@/components/infrastructure/infrastructureChart";
-import InfrastructureBody from "@/components/infrastructure/infrastructureBody";
-import { allInfrastructures } from "@/util/infrastructure_index";
+import LayerProps from "@/components/layer/layerProps";
+import LayerHead from "@/components/layer/layerHead";
+import LayerSummary from "@/components/layer/layerSummary";
+// import LayerChart from "@/components/layer/layerChart";
+import LayerBody from "@/components/layer/layerBody";
+import { allLayers } from "@/util/layer_index";
 
-async function getInfrastructureFromSlug(paramsSlug: string) {
-  const infrastructure = allInfrastructures.find(
-    (infrastructure) => infrastructure.slug === paramsSlug
+async function getLayerFromSlug(paramsSlug: string) {
+  const layer = allLayers.find(
+    (layer) => layer.slug === paramsSlug
   );
-  if (!infrastructure) {
-    console.log("Infrastructure not found: ", paramsSlug);
+  if (!layer) {
+    console.log("Layer not found: ", paramsSlug);
     null;
   }
-  return infrastructure;
+  return layer;
 }
 
-export default async function InfrastructurePage({
+export default async function LayerPage({
   params,
 }: {
   params: { slug: string };
@@ -27,9 +27,9 @@ export default async function InfrastructurePage({
   if (!params) {
     return <div>Params is undefined</div>;
   }
-  console.log("Open page for infrastructure: ", params.slug);
-  const infrastructure = await getInfrastructureFromSlug(params.slug);
-  if (!infrastructure) {
+  console.log("Open page for layer: ", params.slug);
+  const layer = await getLayerFromSlug(params.slug);
+  if (!layer) {
     notFound();
   }
 
@@ -37,21 +37,21 @@ export default async function InfrastructurePage({
     <article className="py-6 prose dark:prose-invert max-w-5xl mx-auto pb-12">
       <div className="grid grid-cols-3 gap-4">
         <div className="col-span-3">
-          <h1 className="mb-2">{infrastructure.title}</h1>
+          <h1 className="mb-2">{layer.title}</h1>
           <hr className="my-4 border-white border-2" />
         </div>
         <div className="col-span-3 md:col-span-2 pr-4">
-          {/* <InfrastructureHead infrastructure={infrastructure as InfrastructureProps} /> */}
+          <LayerHead layer={layer as LayerProps} />
           <div className="mt-8">
-            {/* <InfrastructureSummary infrastructure={infrastructure as InfrastructureProps} /> */}
+            <LayerSummary layer={layer as LayerProps} />
           </div>
         </div>
         <div className="col-span-3 md:col-span-1">
-          {/* <InfrastructureChart infrastructure={infrastructure as InfrastructureProps} /> */}
+          {/* <LayerChart layer={layer as LayerProps} /> */}
         </div>
       </div>
-      <InfrastructureBody
-        infrastructure={infrastructure as InfrastructureProps}
+      <LayerBody
+        layer={layer as LayerProps}
       />
     </article>
   );
