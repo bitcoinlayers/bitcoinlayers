da0f8edf2176bfb22bda759282532f2f8f6411a6
Red <bradley.t.sheehan@gmail.com>
2024-04-10 20:15:07 -0600

    rm coming soon

--- components/homepageTabs/infrastructureTab.tsx
+++ components/homepageTabs/infrastructureTab.tsx
@@ -5,7 +5,7 @@ import InfrastructureTable from "@/components/infrastructure/infrastructureTable
 const InfrastructureTab = () => {
   return (
     <div className="max-w-5xl mx-auto pb-16">
-      <h1 className="my-4 text-xl font-bold text-bitcoin">Coming Soon</h1>
+      {/* <h1 className="my-4 text-xl font-bold text-bitcoin">Coming Soon</h1> */}
       <InfrastructureTable data={allInfrastructures} />
     </div>
   );
