2b3c64313bc63fbfef62294324f4b59ac05d62d3
Red <bradley.t.sheehan@gmail.com>
2024-04-16 17:02:59 -0600

    layer page width

--- app/layers/[slug]/page.tsx
+++ app/layers/[slug]/page.tsx
@@ -32,7 +32,7 @@ export default async function LayerPage({
   }
 
   return (
-    <article className="py-6 prose dark:prose-invert max-w-5xl mx-auto pb-12">
+    <article className="py-6 prose dark:prose-invert max-w-6xl mx-auto pb-12">
       <div className="grid grid-cols-3 gap-4">
         <div className="col-span-3">
           <h1 className="mb-2">{layer.title}</h1>
