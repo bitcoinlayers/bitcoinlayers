81217c14047df8512f4948c4103c7c9c239d6ff7
Red <bradley.t.sheehan@gmail.com>
2024-05-26 12:48:57 -0600

    riskanalysis styling

--- components/layer/layerBodyRiskAnalysis.tsx
+++ components/layer/layerBodyRiskAnalysis.tsx
@@ -19,26 +19,31 @@ const RiskAnalysis: React.FC<{ section: any; riskFactors: string[] }> = ({ secti
   return (
     <>
       {section.content.map((content: any, contentIndex: number) => (
-        <div key={contentIndex} className="flex-col justify-start items-start gap-2 flex">
-          <div className="self-stretch justify-between items-center inline-flex">
-            <div className="body_section">{RISK_FACTOR_CATEGORIES[contentIndex]}</div>
-            <div className="h-8 justify-end items-center gap-2 flex">
-              <div className={`${getRiskColorClass(riskFactors[contentIndex])} text-sm font-medium leading-tight`}>
-                {riskFactors[contentIndex]} risk
-              </div>
-              <div className="w-8 h-8 justify-center items-center flex">
-                <div className={`w-8 h-8 bg-${riskFactors[contentIndex].toLowerCase()}-100 rounded-full`}></div>
-                <div className={`text-center text-${riskFactors[contentIndex].toLowerCase()}-600 text-base font-bold font-['Hack']`}>{contentIndex + 1}</div>
+        <React.Fragment key={contentIndex}>
+          <div className="flex flex-col justify-start items-start gap-2">
+            <div className="self-stretch justify-between items-center inline-flex">
+              <div className="body_section">{RISK_FACTOR_CATEGORIES[contentIndex]}</div>
+              <div className="h-8 justify-end items-center gap-2 flex">
+                <div className={`${getRiskColorClass(riskFactors[contentIndex])} text-sm font-medium leading-tight`}>
+                  {riskFactors[contentIndex]} risk
+                </div>
+                <div className="w-8 h-8 justify-center items-center flex">
+                  <div className={`w-8 h-8 bg-${riskFactors[contentIndex].toLowerCase()}-100 rounded-full`}></div>
+                  <div className={`text-center text-${riskFactors[contentIndex].toLowerCase()}-600 text-base font-bold font-['Hack']`}>{contentIndex + 1}</div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="self-stretch flex-col justify-start items-start gap-2 flex">
-            <div className="self-stretch justify-between items-end inline-flex">
-              <div className="grow shrink basis-0 body_subsection">{content.title}</div>
+            <div className="self-stretch flex-col justify-start items-start gap-2 flex">
+              <div className="self-stretch justify-between items-end inline-flex">
+                <div className="grow shrink basis-0 body_subsection">{content.title}</div>
+              </div>
+              <div className="self-stretch">{parseTextWithLinks(content.content)}</div>
             </div>
-            <div className="self-stretch">{parseTextWithLinks(content.content)}</div>
           </div>
-        </div>
+          {contentIndex < section.content.length - 1 && (
+            <div className="border-b border-slate-300 my-12"></div>
+          )}
+        </React.Fragment>
       ))}
     </>
   );
