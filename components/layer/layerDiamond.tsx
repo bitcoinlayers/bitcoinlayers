3fe19c4df7c7d0873c9922f35450f82ff4021012
Red <bradley.t.sheehan@gmail.com>
2024-05-28 17:27:29 -0600

    risk diamond

---
+++ components/layer/layerDiamond.tsx
@@ -0,0 +1,159 @@
+import React from "react";
+import { Layer } from "./layerProps";
+
+const getRiskColorClass = (riskFactor: string) => {
+  switch (riskFactor) {
+    case "Low":
+      return {
+        bg: "bg-low",
+        fill: "#48E55F",
+      };
+    case "Medium":
+      return {
+        bg: "bg-medium",
+        fill: "#FFC21B",
+      };
+    case "Medium-High":
+      return {
+        bg: "bg-medium-high",
+        fill: "#FF9900",
+      };
+    case "High":
+      return {
+        bg: "bg-high",
+        fill: "#EC0B43",
+      };
+    default:
+      return {
+        bg: "bg-secondary",
+        fill: "#CCCCCC",
+      };
+  }
+};
+
+const LayerDiamond: React.FC<{ layer: Layer }> = ({ layer }) => {
+  const svgDimensions = "w-32 h-32"; // Consistent dimensions for all SVGs
+
+  return (
+    <div className="w-[370px] h-full flex justify-center items-center relative">
+      <div className="left-[243px] top-[195.55px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+        Data Availability
+      </div>
+      <div className="left-0 top-[136.87px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+        Settlement
+      </div>
+      <div className="left-[165.14px] top-0 absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+        Bridge
+      </div>
+      <div className="left-[75.14px] top-[212px] absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+        Network Operators
+      </div>
+
+      <div className={`absolute bottom-[255px] left-[90px] ${svgDimensions}`}>
+        <svg
+          viewBox="0 0 97 97"
+          className="w-full h-full relative flex-col justify-start items-start flex"
+          style={{ fill: getRiskColorClass(layer.riskAnalysis[0].tier).fill }}
+        >
+          <g id="risk-square">
+            <g clipPath="url(#clip0_207_52485)">
+              <rect
+                id="Rectangle 33"
+                x="48.2646"
+                y="0.264648"
+                width="68.215"
+                height="68.215"
+                transform="rotate(45 48.2646 0.264648)"
+              />
+            </g>
+          </g>
+          <defs>
+            <clipPath id="clip0_207_52485">
+              <path d="M44.8672 3.66207C46.7436 1.78572 49.7857 1.78573 51.6621 3.66208L93.1025 45.1025C94.9789 46.9789 94.9789 50.021 93.1025 51.8974L51.6621 93.3378C49.7857 95.2142 46.7436 95.2142 44.8672 93.3378L3.42678 51.8974C1.55043 50.021 1.55044 46.9789 3.42678 45.1025L44.8672 3.66207Z" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+
+      <div className={`absolute bottom-[185px] right-[85px] ${svgDimensions}`}>
+        <svg
+          viewBox="0 0 97 97"
+          className="w-full h-full relative flex-col justify-start items-start flex"
+          style={{ fill: getRiskColorClass(layer.riskAnalysis[1].tier).fill }}
+        >
+          <g id="risk-square">
+            <g clipPath="url(#clip0_207_52485)">
+              <rect
+                id="Rectangle 33"
+                x="48.2646"
+                y="0.264648"
+                width="68.215"
+                height="68.215"
+                transform="rotate(45 48.2646 0.264648)"
+              />
+            </g>
+          </g>
+          <defs>
+            <clipPath id="clip0_207_52485">
+              <path d="M44.8672 3.66207C46.7436 1.78572 49.7857 1.78573 51.6621 3.66208L93.1025 45.1025C94.9789 46.9789 94.9789 50.021 93.1025 51.8974L51.6621 93.3378C49.7857 95.2142 46.7436 95.2142 44.8672 93.3378L3.42678 51.8974C1.55043 50.021 1.55044 46.9789 3.42678 45.1025L44.8672 3.66207Z" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+
+      <div className={`absolute bottom-[113px] left-[90px] ${svgDimensions}`}>
+        <svg
+          viewBox="0 0 97 97"
+          className="w-full h-full relative flex-col justify-start items-start flex"
+          style={{ fill: getRiskColorClass(layer.riskAnalysis[2].tier).fill }}
+        >
+          <g id="risk-square">
+            <g clipPath="url(#clip0_207_52485)">
+              <rect
+                id="Rectangle 33"
+                x="48.2646"
+                y="0.264648"
+                width="68.215"
+                height="68.215"
+                transform="rotate(45 48.2646 0.264648)"
+              />
+            </g>
+          </g>
+          <defs>
+            <clipPath id="clip0_207_52485">
+              <path d="M44.8672 3.66207C46.7436 1.78572 49.7857 1.78573 51.6621 3.66208L93.1025 45.1025C94.9789 46.9789 94.9789 50.021 93.1025 51.8974L51.6621 93.3378C49.7857 95.2142 46.7436 95.2142 44.8672 93.3378L3.42678 51.8974C1.55043 50.021 1.55044 46.9789 3.42678 45.1025L44.8672 3.66207Z" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+
+      <div className={`absolute bottom-[185px] left-[23px] ${svgDimensions}`}>
+        <svg
+          viewBox="0 0 97 97"
+          className="w-full h-full relative flex-col justify-start items-start flex"
+          style={{ fill: getRiskColorClass(layer.riskAnalysis[3].tier).fill }}
+        >
+          <g id="risk-square">
+            <g clipPath="url(#clip0_207_52485)">
+              <rect
+                id="Rectangle 33"
+                x="48.2646"
+                y="0.264648"
+                width="68.215"
+                height="68.215"
+                transform="rotate(45 48.2646 0.264648)"
+              />
+            </g>
+          </g>
+          <defs>
+            <clipPath id="clip0_207_52485">
+              <path d="M44.8672 3.66207C46.7436 1.78572 49.7857 1.78573 51.6621 3.66208L93.1025 45.1025C94.9789 46.9789 94.9789 50.021 93.1025 51.8974L51.6621 93.3378C49.7857 95.2142 46.7436 95.2142 44.8672 93.3378L3.42678 51.8974C1.55043 50.021 1.55044 46.9789 3.42678 45.1025L44.8672 3.66207Z" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default LayerDiamond;
--- components/layer/layerOverview.tsx
+++ components/layer/layerOverview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Layer } from "./layerProps";
 import Image from "next/image";
+import LayerDiamond from "./layerDiamond";
 
 const LinkButton = ({
   href,
@@ -18,37 +19,42 @@ const LinkButton = ({
   </a>
 );
 
+const LayerOverview: React.FC<{ layer: Layer }> = ({ layer }) => {
+  return (
+    <div className="flex justify-between pt-6 gap-4 mb-12">
+      <div className="flex flex-col w-13/20 space-y-10">
+        <Categories layer={layer} />
+        <Description layer={layer} />
+        <div className="border-t border-stroke_secondary"></div>
+        <Links layer={layer} />
+      </div>
+      <div className="w-6/20">
+        {/* <Radar /> */}
+        <LayerDiamond layer={layer} />
+      </div>
+    </div>
+  );
+};
+
+export default LayerOverview;
+
 const Categories: React.FC<{ layer: Layer }> = ({ layer }) => {
   return (
     <div className="flex justify-between w-full">
       <div className="flex-col justify-center items-start">
-        <div className="text-text_primary text-sm leading-tight">
-          Status
-        </div>
-        <div className="text-text_header">
-          {layer.live}
-        </div>
+        <div className="text-text_primary text-sm leading-tight">Status</div>
+        <div className="text-text_header">{layer.live}</div>
       </div>
       <div className="flex-col justify-center items-start">
-        <div className="text-text_primary text-sm leading-tight">
-          Type
-        </div>
-        <div className="text-text_header">
-          {layer.layerType}
-        </div>
+        <div className="text-text_primary text-sm leading-tight">Type</div>
+        <div className="text-text_header">{layer.layerType}</div>
       </div>
       <div className="flex-col justify-center items-start">
-        <div className="text-text_primary text-sm leading-tight">
-          Fee Token
-        </div>
-        <div className="text-text_header">
-          {layer.feeToken}
-        </div>
+        <div className="text-text_primary text-sm leading-tight">Fee Token</div>
+        <div className="text-text_header">{layer.feeToken}</div>
       </div>
       <div className="flex-col justify-center items-start">
-        <div className="text-text_primary text-sm leading-tight">
-          TVL
-        </div>
+        <div className="text-text_primary text-sm leading-tight">TVL</div>
         <div className="text-zinc-800 text-base font-normal leading-normal">
           ₿ {layer.btcLocked}
         </div>
@@ -58,7 +64,9 @@ const Categories: React.FC<{ layer: Layer }> = ({ layer }) => {
 };
 
 const Description: React.FC<{ layer: Layer }> = ({ layer }) => {
-  return <div className="self-stretch text-text_secondary">{layer.description}</div>;
+  return (
+    <div className="self-stretch text-text_secondary">{layer.description}</div>
+  );
 };
 
 const Links: React.FC<{ layer: Layer }> = ({ layer }) => {
@@ -138,64 +146,46 @@ const Links: React.FC<{ layer: Layer }> = ({ layer }) => {
   );
 };
 
-const Radar: React.FC = () => {
-  return (
-    <div className="self-stretch flex-col justify-between items-center inline-flex">
-      <div className="h-80 pl-9 pr-[37.14px] pt-[9px] pb-3 justify-center items-center inline-flex">
-        <div className="w-[298.86px] h-[298.97px] relative">
-          <div className="w-[272px] h-[272px] left-[15px] top-[15px] absolute">
-            <div className="left-[130px] top-[113.90px] absolute text-slate-500 text-[8px] font-normal">
-              20
-            </div>
-            <div className="left-[130px] top-[86.70px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-              40
-            </div>
-            <div className="left-[130px] top-[59.50px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-              60
-            </div>
-            <div className="left-[130px] top-[32.30px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-              80
-            </div>
-            <div className="left-[129px] top-[5.10px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-              100
-            </div>
-            <div className="w-2 h-2 left-[132px] top-[64px] absolute bg-amber-400 rounded-full"></div>
-            <div className="w-2 h-2 left-[119px] top-[132px] absolute bg-red-500 rounded-full"></div>
-            <div className="w-2 h-2 left-[131px] top-[170px] absolute bg-red-500 rounded-full"></div>
-            <div className="w-2 h-2 left-[169px] top-[132px] absolute bg-red-500 rounded-full"></div>
-          </div>
-          <div className="left-[243px] top-[195.55px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-            Settlement
-          </div>
-          <div className="left-0 top-[136.87px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-            Bridge
-          </div>
-          <div className="left-[165.14px] top-0 absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-            Data Availability
-          </div>
-          <div className="left-[75.14px] top-[212px] absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-            Network Operators
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const LayerOverview: React.FC<{ layer: Layer }> = ({ layer }) => {
-  return (
-    <div className="flex justify-between pt-6 gap-4 mb-12">
-      <div className="flex flex-col w-13/20 space-y-10">
-        <Categories layer={layer} />
-        <Description layer={layer} />
-        <div className="border-t border-stroke_secondary"></div>
-        <Links layer={layer} />
-      </div>
-      <div className="w-6/20">
-        <Radar />
-      </div>
-    </div>
-  );
-};
-
-export default LayerOverview;
+// const Radar: React.FC = () => {
+//   return (
+//     <div className="self-stretch flex-col justify-between items-center inline-flex">
+//       <div className="h-80 pl-9 pr-[37.14px] pt-[9px] pb-3 justify-center items-center inline-flex">
+//         <div className="w-[298.86px] h-[298.97px] relative">
+//           <div className="w-[272px] h-[272px] left-[15px] top-[15px] absolute">
+//             <div className="left-[130px] top-[113.90px] absolute text-slate-500 text-[8px] font-normal">
+//               20
+//             </div>
+//             <div className="left-[130px] top-[86.70px] absolute text-slate-500 text-[8px] font-normal leading-snug">
+//               40
+//             </div>
+//             <div className="left-[130px] top-[59.50px] absolute text-slate-500 text-[8px] font-normal leading-snug">
+//               60
+//             </div>
+//             <div className="left-[130px] top-[32.30px] absolute text-slate-500 text-[8px] font-normal leading-snug">
+//               80
+//             </div>
+//             <div className="left-[129px] top-[5.10px] absolute text-slate-500 text-[8px] font-normal leading-snug">
+//               100
+//             </div>
+//             <div className="w-2 h-2 left-[132px] top-[64px] absolute bg-amber-400 rounded-full"></div>
+//             <div className="w-2 h-2 left-[119px] top-[132px] absolute bg-red-500 rounded-full"></div>
+//             <div className="w-2 h-2 left-[131px] top-[170px] absolute bg-red-500 rounded-full"></div>
+//             <div className="w-2 h-2 left-[169px] top-[132px] absolute bg-red-500 rounded-full"></div>
+//           </div>
+//           <div className="left-[243px] top-[195.55px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+//             Settlement
+//           </div>
+//           <div className="left-0 top-[136.87px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+//             Bridge
+//           </div>
+//           <div className="left-[165.14px] top-0 absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+//             Data Availability
+//           </div>
+//           <div className="left-[75.14px] top-[212px] absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
+//             Network Operators
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
