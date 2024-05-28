044f0d9e6e1a260df58bae40bc6b2b8b6db5af2b
janusz <82240624+januszgrze@users.noreply.github.com>
2024-05-28 19:49:01 +0100

    Update layerTableRisks.tsx

--- components/layer/layerTableRisks.tsx
+++ components/layer/layerTableRisks.tsx
@@ -64,7 +64,7 @@ const LayerTableRisks = ({ data }: Props) => {
               Data Availability
             </th>
             <th className="px-6 py-6 font-medium text-text_table_header table_header border-t border-stroke_tertiary">
-              Network Operators
+              Network Operator
             </th>
             <th className="px-6 py-6 font-medium text-text_table_header table_header border-t border-r border-stroke_tertiary first:rounded-tr-xl">
               Settlement Assurance
