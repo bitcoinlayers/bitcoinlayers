ac7690ea927199b3e07676a7565d1e4e57052e5c
Janusz <82240624+januszgrze@users.noreply.github.com>
2024-06-13 14:36:03 +0200

    Update layer_index.tsx
    remove some projects from website

--- util/layer_index.tsx
+++ util/layer_index.tsx
@@ -8,18 +8,14 @@ import mercurylayerJson from "../content/layers/mercurylayer.json";
 import rootstockJson from "../content/layers/rootstock.json";
 import stacksJson from "../content/layers/stacks.json";
 import alpenJson from "../content/layers/alpen.json";
-import barknetJson from "../content/layers/barknet.json";
 import bisonJson from "../content/layers/bison.json";
-import bnzkJson from "../content/layers/bnzk.json";
 import bobJson from "../content/layers/bob.json";
 import botanixJson from "../content/layers/botanix.json";
 import bsquaredJson from "../content/layers/bsquared.json";
 import citreaJson from "../content/layers/citrea.json";
-import flashJson from "../content/layers/flash.json";
 import rgbJson from "../content/layers/rgb.json";
 import heliosphereJson from "../content/layers/heliosphere.json";
 import libreJson from "../content/layers/libre.json";
-import lumibitJson from "../content/layers/lumibit.json";
 import merlinJson from "../content/layers/merlin.json";
 import rifJson from "../content/layers/rif.json";
 import rolluxJson from "../content/layers/rollux.json";
@@ -45,20 +41,16 @@ const mercurylayer: Layer = mercurylayerJson as Layer;
 const rootstock: Layer = rootstockJson as Layer;
 const stacks: Layer = stacksJson as Layer;
 const alpen: Layer = alpenJson as Layer;
-const barknet: Layer = barknetJson as Layer;
 const bison: Layer = bisonJson as Layer;
-const bnzk: Layer = bnzkJson as Layer;
 // @ts-ignore
 const bob: Layer = bobJson as Layer;
 const botanix: Layer = botanixJson as Layer;
 // @ts-ignore
 const bsquared: Layer = bsquaredJson as Layer;
 const citrea: Layer = citreaJson as Layer;
-const flash: Layer = flashJson as Layer;
 const rgb: Layer = rgbJson as Layer;
 const heliosphere: Layer = heliosphereJson as Layer;
 const libre: Layer = libreJson as Layer;
-const lumibit: Layer = lumibitJson as Layer;
 const merlin: Layer = merlinJson as Layer;
 const rif: Layer = rifJson as Layer;
 const rollux: Layer = rolluxJson as Layer;
@@ -86,18 +78,14 @@ export const allLayers: Layer[] = [
     rootstock,
     stacks,
     alpen,
-    barknet,
     bison,
-    bnzk,
     bob,
     botanix,
     bsquared,
     citrea,
-    flash,
     rgb,
     heliosphere,
     libre,
-    lumibit,
     merlin,
     rif,
     rollux,
