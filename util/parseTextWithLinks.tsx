413c159f75c72f24194a3bb2fb52ac68b5fcd2eb
Red <bradley.t.sheehan@gmail.com>
2024-07-01 08:46:25 -0600

    add newlines to content pages

--- util/parseTextWithLinks.tsx
+++ util/parseTextWithLinks.tsx
@@ -2,21 +2,29 @@ import Link from "next/link";
 import React from "react";
 
 export const parseTextWithLinks = (text: string) => {
-    const urlRegex = /(\bhttps?:\/\/[^\s]+)/g;
-    const parts = text.split(urlRegex);
+    const urlRegex = /\[Link\]\((https?:\/\/[^\s]+)\)/g;
+    const newlineRegex = /\n/g;
 
-    return parts.map((part, index) =>
-        urlRegex.test(part) ? (
-            <Link
-                key={index}
-                href={part}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                {part}
-            </Link>
-        ) : (
-            part
-        ),
-    );
+    const parts = text.split(newlineRegex).map((line, lineIndex) => (
+        <React.Fragment key={lineIndex}>
+            {line.split(urlRegex).map((part, index) => {
+                const match = part.match(urlRegex);
+                return match ? (
+                    <Link
+                        key={index}
+                        href={match[1]}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {match[1]}
+                    </Link>
+                ) : (
+                    part
+                );
+            })}
+            {lineIndex < text.split(newlineRegex).length - 1 && <br />}
+        </React.Fragment>
+    ));
+
+    return parts;
 };
