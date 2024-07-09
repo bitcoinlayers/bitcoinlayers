289cf4c21695818a36264843b35e600efc900c23
Red <bradley.t.sheehan@gmail.com>
2024-07-09 12:25:29 -0600

    WIP

--- components/layer/layerDiamond.tsx
+++ components/layer/layerDiamond.tsx
@@ -19,8 +19,6 @@ const Tooltip: React.FC<{ title: string; style: React.CSSProperties }> = ({
 );
 
 const LayerDiamond: React.FC<{ layer: Layer }> = ({ layer }) => {
-    const svgDimensions = "w-36 h-36";
-
     const [tooltip, setTooltip] = useState<{
         title: string;
         style: React.CSSProperties;
@@ -45,59 +43,65 @@ const LayerDiamond: React.FC<{ layer: Layer }> = ({ layer }) => {
         setTooltip(null);
     };
 
+    const containerSize = 350;
+    const svgDivSize = containerSize / 2;
+    const svgSize = 155;
+
     const renderDiamond = (
         riskFactor: string,
         title: string,
-        positionStyle: string,
+        positionTop: number,
+        positionLeft: number,
         IconComponent: React.FC<{ fill: string }>,
     ) => {
         const bgColor = getRiskColorBackground(riskFactor);
         const fillColor = getRiskColorIcon(riskFactor);
         return (
-            <div className={`absolute ${positionStyle} ${svgDimensions}`}>
+            <div
+                className="absolute border-2"
+                style={{
+                    width: svgDivSize,
+                    height: svgDivSize,
+                    top: positionTop,
+                    left: positionLeft,
+                }}
+            >
                 <svg
-                    viewBox="0 0 106 106"
-                    className="w-full h-full relative flex-col justify-start items-start flex"
+                    viewBox={`0 0 ${svgSize * 2} ${svgSize * 2}`}
+                    width={svgDivSize}
+                    height={svgDivSize}
+                    className="relative flex-col justify-start items-start flex"
                     onMouseEnter={(e) => handleMouseEnter(e, title)}
                     onMouseLeave={handleMouseLeave}
                 >
                     <rect
-                        x="48.2646"
-                        y="0.264648"
-                        width="68.215"
-                        height="68.215"
-                        transform="rotate(45 48.2646 0.264648)"
+                        x={svgSize / 2}
+                        y={svgSize / 2}
+                        width={svgSize}
+                        height={svgSize}
+                        transform={`rotate(45 ${svgSize / 2} ${svgSize / 2})`}
                         style={{ fill: bgColor }}
-                        rx="10"
-                        ry="10"
+                        rx={svgSize / 10}
+                        ry={svgDivSize / 10}
                     />
-                    <foreignObject
-                        x="14"
-                        y="14"
-                        width="70"
-                        height="70"
-                        className="flex justify-center items-center"
-                    >
-                        <div className="w-full h-full flex justify-center items-center">
-                            <IconComponent fill={fillColor} />
-                        </div>
-                    </foreignObject>
                 </svg>
             </div>
         );
     };
 
+    const containerClassName = `lg:w-[${containerSize}px] h-[${containerSize}px] bg-black lg:h-full flex justify-center items-center relative ml-6 lg:ml-0 z-30`;
+
     return (
-        <div className="lg:w-[350px] h-[350px] bg-black lg:h-full flex justify-center items-center relative ml-6 lg:ml-0 z-30">
+        <div className={containerClassName}>
             {tooltip && <Tooltip title={tooltip.title} style={tooltip.style} />}
 
-            <div className="left-[10px] top-[140px] absolute origin-top-left -rotate-45 text-left text-slate-600 text-xs font-medium leading-none">
+            <div className="left-[10px] top-[140px] absolute origin-top-left text-left text-slate-600 text-xs font-medium leading-none">
                 BRIDGE
             </div>
             <div className="left-[50%] translate-x-[-50%] top-[10px] absolute origin-top-left text-center text-slate-600 text-xs font-medium leading-none w-[80px]">
                 DATA AVAILABILITY
             </div>
-            <div className="right-[10px] top-[140px] absolute origin-top-right rotate-45 text-right text-slate-600 text-xs font-medium leading-none">
+            <div className="right-[10px] top-[140px] absolute origin-top-right text-right text-slate-600 text-xs font-medium leading-none">
                 OPERATORS
             </div>
             <div className="left-[50%] translate-x-[-50%] top-[290px] absolute origin-top-left text-center text-slate-600 text-xs font-medium leading-none w-[80px]">
@@ -107,25 +111,29 @@ const LayerDiamond: React.FC<{ layer: Layer }> = ({ layer }) => {
             {renderDiamond(
                 layer.riskAnalysis[0].tier,
                 layer.riskAnalysis[0].title,
-                "top-[136.87px] left-[0px]",
+                svgDivSize * 0.5,
+                svgDivSize * 0.0,
                 RiskIconBridge,
             )}
             {renderDiamond(
                 layer.riskAnalysis[1].tier,
                 layer.riskAnalysis[1].title,
-                "top-[64px] left-[75px]",
+                svgDivSize * 0.0,
+                svgDivSize * 0.5,
                 RiskIconDA,
             )}
             {renderDiamond(
                 layer.riskAnalysis[2].tier,
                 layer.riskAnalysis[2].title,
-                "top-[136.87px] left-[150px]",
+                svgDivSize * 0.5,
+                svgDivSize * 1.0,
                 RiskIconOperators,
             )}
             {renderDiamond(
                 layer.riskAnalysis[3].tier,
                 layer.riskAnalysis[3].title,
-                "top-[210px] left-[75px]",
+                svgDivSize * 1.0,
+                svgDivSize * 0.5,
                 RiskIconSettlement,
             )}
         </div>
