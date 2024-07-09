1778b3777e957786f6c277ca00e379b0d025806a
Red <bradley.t.sheehan@gmail.com>
2024-07-09 13:14:13 -0600

    overview spacing

--- components/layer/layerOverview.tsx
+++ components/layer/layerOverview.tsx
@@ -25,14 +25,16 @@ const LayerOverview: React.FC<{ layer: Layer }> = ({ layer }) => {
             id="overview"
             className="flex lg:flex-row flex-col justify-between pt-6 gap-4 mb-12"
         >
-            <div className="flex flex-col w-13/20 space-y-10 px-4">
+            <div
+                className="flex flex-col space-y-10 px-4 flex-grow"
+                style={{ width: "25%" }}
+            >
                 <Categories layer={layer} />
                 <Description layer={layer} />
                 <div className="border-t border-stroke_secondary"></div>
                 <Links layer={layer} />
             </div>
-            <div className="lg:w-6/20 w-full mt-4 lg:mt-0">
-                {/* <Radar /> */}
+            <div className="mt-4 lg:mt-0" style={{ width: "350px" }}>
                 <LayerDiamond layer={layer} />
             </div>
         </section>
@@ -84,7 +86,7 @@ const Description: React.FC<{ layer: Layer }> = ({ layer }) => {
 
 const Links: React.FC<{ layer: Layer }> = ({ layer }) => {
     return (
-        <div className="self-stretch flex lg:justify-start justify-center items-start gap-4 flex-wrap">
+        <div className="self-stretch flex lg:justify-start justify-center items-start gap-2 flex-wrap">
             <LinkButton href={String(layer.links[0])}>
                 <div className="bg-white/opacity-0 flex-col justify-center items-center inline-flex">
                     <div className="w-3.5 h-3.5 relative">
@@ -158,47 +160,3 @@ const Links: React.FC<{ layer: Layer }> = ({ layer }) => {
         </div>
     );
 };
-
-// const Radar: React.FC = () => {
-//   return (
-//     <div className="self-stretch flex-col justify-between items-center inline-flex">
-//       <div className="h-80 pl-9 pr-[37.14px] pt-[9px] pb-3 justify-center items-center inline-flex">
-//         <div className="w-[298.86px] h-[298.97px] relative">
-//           <div className="w-[272px] h-[272px] left-[15px] top-[15px] absolute">
-//             <div className="left-[130px] top-[113.90px] absolute text-slate-500 text-[8px] font-normal">
-//               20
-//             </div>
-//             <div className="left-[130px] top-[86.70px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-//               40
-//             </div>
-//             <div className="left-[130px] top-[59.50px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-//               60
-//             </div>
-//             <div className="left-[130px] top-[32.30px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-//               80
-//             </div>
-//             <div className="left-[129px] top-[5.10px] absolute text-slate-500 text-[8px] font-normal leading-snug">
-//               100
-//             </div>
-//             <div className="w-2 h-2 left-[132px] top-[64px] absolute bg-amber-400 rounded-full"></div>
-//             <div className="w-2 h-2 left-[119px] top-[132px] absolute bg-red-500 rounded-full"></div>
-//             <div className="w-2 h-2 left-[131px] top-[170px] absolute bg-red-500 rounded-full"></div>
-//             <div className="w-2 h-2 left-[169px] top-[132px] absolute bg-red-500 rounded-full"></div>
-//           </div>
-//           <div className="left-[243px] top-[195.55px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-//             Settlement
-//           </div>
-//           <div className="left-0 top-[136.87px] absolute origin-top-left -rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-//             Bridge
-//           </div>
-//           <div className="left-[165.14px] top-0 absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-//             Data Availability
-//           </div>
-//           <div className="left-[75.14px] top-[212px] absolute origin-top-left rotate-45 text-center text-slate-600 text-xs font-medium leading-none">
-//             Network Operators
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
