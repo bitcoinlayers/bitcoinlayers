58df68bbc6a46b1757b1225004f193075fea85cf
Red <bradley.t.sheehan@gmail.com>
2024-07-18 14:26:52 -0600

    Glossary

--- app/glossary/page.tsx
+++ app/glossary/page.tsx
@@ -1,30 +1,104 @@
 import React from "react";
 
+type GlossaryItem = {
+    term: string;
+    definition: string;
+};
+
+type GlossaryData = {
+    [key: string]: GlossaryItem[];
+};
+
+const glossaryData: GlossaryData = {
+    A: [
+        {
+            term: "Anchored Sidechain",
+            definition:
+                "A sidechain that inherits Bitcoin finality and does not reorg unless the Bitcoin mainchain reorgs.",
+        },
+    ],
+    // B: [{ term: "xx", definition: "yy" }],
+    // C: [],
+    // D: [],
+    // E: [],
+    // F: [],
+    // G: [],
+    // H: [],
+    // I: [],
+    // J: [],
+    // K: [],
+    // L: [],
+    // M: [],
+    // N: [],
+    // O: [],
+    // P: [],
+    // Q: [],
+    R: [
+        {
+            term: "Rollup",
+            definition:
+                "A blockchain that posts its latest state root, and enough transaction data to reconstruct its state from genesis, to Bitcoin",
+        },
+    ],
+    S: [
+        {
+            term: "Sidechain",
+            definition:
+                "A blockchain with sovereign execution, consensus, data availability, and security (i.e., an L1) that is in some way aligned with Bitcoin",
+        },
+        {
+            term: "Spiderchain",
+            definition:
+                "A sidechain protocol that leverages a variety of rotating multi-sigs to secure the BTC that is deposited into the sidechain",
+        },
+
+        {
+            term: "State channel",
+            definition:
+                "A system where part of the blockchain's state is locked into a multi-signature contract between a number of counterparties. These counterparties engage in transactions, related to this state, amongst themselves and keep a record of state transitions locally",
+        },
+        {
+            term: "Statechain",
+            definition:
+                "A protocol where users enter a 2-2 multisig with a federated entity and transfer ownership of a UTXO to an intended recipient by providing them a statechain private key for that specific UTXO",
+        },
+    ],
+    // T: [],
+    // U: [],
+    V: [
+        {
+            term: "Validity rollup",
+            definition:
+                "A rollup where a prover submits a validity proof to a verifier contract on the L1 proving that the state transition was executed correctly",
+        },
+    ],
+    // W: [],
+    // X: [],
+    // Y: [],
+    // Z: [],
+    // template: [{ term: "XXX", definition: "YYY" },],
+};
+
 const GlossaryPage: React.FC = () => {
-    function InfoBox({
-        question,
-        answer,
-    }: {
-        question: string;
-        answer: string;
-    }) {
-        return (
-            <div className="bg-white rounded-xl border border-slate-300 flex flex-col justify-center items-start gap-4 p-8">
-                <div className="flex items-center gap-3">
-                    <div className="text-3xl font-light text-zinc-800 leading-9">
-                        {question}
-                    </div>
-                </div>
-                <div className="flex flex-col justify-center items-start gap-8 w-full">
-                    <div className="flex flex-col justify-start items-start gap-2 w-full">
-                        <div className="text-base font-normal text-slate-500 leading-normal">
-                            {answer}
+    const renderGlossarySection = (letter: string) => (
+        <div id={letter} className="mb-8 flex flex-col lg:flex-row items-start">
+            <div className="font-black text-text_tertiary font-playfair-display text-10xl flex-shrink-0 w-24 leading-none">
+                {letter}
+            </div>
+            <div className="flex-grow bg-white rounded-xl border border-slate-300 p-4">
+                {glossaryData[letter].map((item, index) => (
+                    <div key={index} className="m-2 gap-4">
+                        <div className="special_header text-3xl font-light text-brand leading-9 mb-4">
+                            {item.term}
+                        </div>
+                        <div className="text-base font-normal text-slate-500 leading-normal mb-4">
+                            {item.definition}
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
-        );
-    }
+        </div>
+    );
 
     return (
         <article className="flex flex-col min-h-screen max-w-5xl mx-auto pt-16">
@@ -36,19 +110,24 @@ const GlossaryPage: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col gap-8 w-full">
-                    <div className="flex flex-col gap-12 w-full rounded-md">
-                        Coming soon.
-                        {/* <div className="flex flex-col gap-8 w-full">
-              <InfoBox
-                question="The Project"
-                answer="Bitcoin Layers is an educational tool that helps users understand the risks associated with various protocols that support Bitcoin and BTC the asset. We analyze risk primarily for protocols that claim to be a Bitcoin L2. The project is free and open-source under an MIT license. Anyone is welcome to use our code and risk framework for their own project."
-              />
-              <InfoBox
-                question="The Team"
-                answer="Bitcoin Layers is currently managed on a volunteer basis by Januszg (a pseudonymous contributor) and Red Sheehan from Messari. Januszg has experience leading marketing functions at well-known projects in the privacy and scaling space. Red is a crypto research analyst specializing in base layer network architectures."
-              />
-            </div> */}
+                <div className="flex flex-col lg:flex-row w-full">
+                    <nav className="lg:w-1/8 lg:mb-8 mb-4 mr-12 lg:pr-4">
+                        <div className="flex lg:flex-col flex-row lg:gap-4 gap-2 lg:sticky lg:top-16">
+                            {Object.keys(glossaryData).map((letter) => (
+                                <a
+                                    key={letter}
+                                    href={`#${letter}`}
+                                    className="text-lg font-medium text-text_primary hover:text-brand"
+                                >
+                                    {letter}
+                                </a>
+                            ))}
+                        </div>
+                    </nav>
+                    <div className="w-full">
+                        {Object.keys(glossaryData).map((letter) =>
+                            renderGlossarySection(letter),
+                        )}
                     </div>
                 </div>
             </div>
