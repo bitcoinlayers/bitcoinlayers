0da598b1f80470f007e59d7a659cc080d547764a
Red <bradley.t.sheehan@gmail.com>
2024-07-19 15:39:25 -0600

    update layersupcoming page

--- app/upcominglayers/page.tsx
+++ app/upcominglayers/page.tsx
@@ -11,15 +11,18 @@ export default function Home() {
     );
 
     const layerHeaders = [
-        { name: "Name", showSorting: true },
-        { name: "Risk", showSorting: false },
-        { name: "Type", showSorting: true },
-        { name: "Status", showSorting: true },
-        // { name: "Type", filterOptions: ["Sidechain", "State Channel", "Rollup"] },
-        // { name: "Status", filterOptions: ["Mainnet", "Testnet", "Announced"] },
-        { name: "Unit of Account", showSorting: true },
-        { name: "BTC Locked", showSorting: true },
+        { name: "Name", showSorting: true, mobileLabel: "Name" },
+        { name: "Risk", showSorting: false, mobileLabel: "Risk" },
+        { name: "Type", showSorting: true, mobileLabel: "Type" },
+        { name: "Status", showSorting: true, mobileLabel: "Status" },
+        {
+            name: "Unit of Account",
+            showSorting: true,
+            mobileLabel: "Unit",
+        },
+        { name: "BTC Locked", showSorting: true, mobileLabel: "BTC" },
     ];
+
     return (
         <div className="mx-auto">
             <Hero />
