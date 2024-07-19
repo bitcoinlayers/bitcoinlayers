78d0dfca041092c0f91ef473126f9d30347ac8b3
Red <bradley.t.sheehan@gmail.com>
2024-07-19 15:34:46 -0600

    new tabkey options

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import React, { useState } from "react";
 import { allLayers } from "@/util/layer_index";
 import LayerTable from "@/components/tables/layerTable";
-import LayerTableRiskView from "@/components/tables/layerTableRiskView";
+import LayerTableAll from "@/components/tables/layerTableAll";
+// import LayerTableRiskView from "@/components/tables/layerTableRiskView"; //risks: <LayerTableRiskView />,
 import Hero from "@/components/hero";
 
-type TabKey = "overview" | "risks";
+type TabKey = "liveLayersOnly" | "allLayers";
 
 export default function Home() {
-    const [activeTab, setActiveTab] = useState<TabKey>("overview");
+    const [activeTab, setActiveTab] = useState<TabKey>("liveLayersOnly");
 
     const sortedLayers = allLayers.sort((a, b) =>
         a.title.toLowerCase().localeCompare(b.title.toLowerCase()),
@@ -31,8 +32,10 @@ export default function Home() {
     ];
 
     const tabComponents = {
-        overview: <LayerTable data={sortedLayers} headers={layerHeaders} />,
-        risks: <LayerTableRiskView />,
+        liveLayersOnly: (
+            <LayerTable data={sortedLayers} headers={layerHeaders} />
+        ),
+        allLayers: <LayerTableAll data={sortedLayers} headers={layerHeaders} />,
     };
 
     const handleTabClick = (tab: TabKey) => {
@@ -46,39 +49,39 @@ export default function Home() {
                 <div className="justify-start items-start gap-4 inline-flex">
                     <div
                         className={`h-[30px] px-4 py-[5px] rounded-full border-2 justify-center items-center gap-1.5 flex cursor-pointer ${
-                            activeTab === "overview"
+                            activeTab === "liveLayersOnly"
                                 ? "bg-white border-orange-600"
                                 : "border-slate-300"
                         }`}
-                        onClick={() => handleTabClick("overview")}
+                        onClick={() => handleTabClick("liveLayersOnly")}
                     >
                         <div
                             className={`text-center text-sm font-medium leading-tight ${
-                                activeTab === "overview"
+                                activeTab === "liveLayersOnly"
                                     ? "text-orange-600"
                                     : "text-slate-600"
                             }`}
                         >
-                            Overview
+                            Live Layers
                         </div>
                     </div>
                     <div
                         className={`h-[30px] rounded-full border-2 justify-center items-center gap-1 flex cursor-pointer ${
-                            activeTab === "risks"
+                            activeTab === "allLayers"
                                 ? "bg-white border-orange-600"
                                 : "border-slate-300"
                         }`}
-                        onClick={() => handleTabClick("risks")}
+                        onClick={() => handleTabClick("allLayers")}
                     >
                         <div className="grow shrink basis-0 h-[30px] px-4 py-[5px] justify-center items-center gap-1.5 flex">
                             <div
                                 className={`text-center text-sm font-medium leading-tight ${
-                                    activeTab === "risks"
+                                    activeTab === "allLayers"
                                         ? "text-orange-600"
                                         : "text-slate-600"
                                 }`}
                             >
-                                Risks
+                                All Layers
                             </div>
                         </div>
                     </div>
