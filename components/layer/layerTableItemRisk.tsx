e4b5999285f7dfd8842d3ee15ebd9f810bddd588
Michael Boland <michael@boland.co>
2024-09-26 15:13:58 -0500

    Fix error

--- components/layer/layerTableItemRisk.tsx
+++ components/layer/layerTableItemRisk.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Layer } from "@/components/layer/layerProps";
 import { getRiskColorBackground, getRiskColorIcon } from "@/util/riskColors";
 import RiskSnapshot from "./riskSnapshot";
@@ -6,6 +5,11 @@ import RiskIconBridge from "@/components/icons/RiskIconBridge";
 import RiskIconDA from "@/components/icons/RiskIconDA";
 import RiskIconOperators from "@/components/icons/RiskIconOperators";
 import RiskIconSettlement from "@/components/icons/RiskIconSettlement";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "../ui/hover-card";
 
 interface RiskProps {
     layer: Layer;
@@ -13,19 +17,6 @@ interface RiskProps {
 
 const Risk: React.FC<RiskProps> = ({ layer }) => {
     const riskLevels = layer.riskAnalysis;
-    const [hovered, setHovered] = useState(false);
-    const [hoverPosition, setHoverPosition] = useState({ top: 80, left: 0 });
-
-    const handleMouseEnter = (event: React.MouseEvent<HTMLDivElement>) => {
-        const { top, left, width } =
-            event.currentTarget.getBoundingClientRect();
-        setHoverPosition({ top, left: left + width + 20 });
-        setHovered(true);
-    };
-
-    const handleMouseLeave = () => {
-        setHovered(false);
-    };
 
     const RiskIcon = ({
         riskFactor,
@@ -47,33 +38,34 @@ const Risk: React.FC<RiskProps> = ({ layer }) => {
     };
 
     return (
-        <div
-            className="relative"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-        >
-            <div className="lg:w-44 w-34 lg:p-4 p-2 justify-start items-center gap-4 inline-flex lg:gap-4 gap-1">
-                <RiskIcon
-                    riskFactor={riskLevels[0]?.tier}
-                    IconComponent={RiskIconBridge}
-                />
-                <RiskIcon
-                    riskFactor={riskLevels[1]?.tier}
-                    IconComponent={RiskIconDA}
-                />
-                <RiskIcon
-                    riskFactor={riskLevels[2]?.tier}
-                    IconComponent={RiskIconOperators}
-                />
-                <RiskIcon
-                    riskFactor={riskLevels[3]?.tier}
-                    IconComponent={RiskIconSettlement}
-                />
-            </div>
-            {hovered && (
-                <RiskSnapshot layer={layer} hoverPosition={hoverPosition} />
-            )}
-        </div>
+        <HoverCard openDelay={300}>
+            <HoverCardTrigger
+            // onClick={isMobile ? toggleCard : undefined}
+            >
+                {" "}
+                <div className="lg:w-44 w-34 lg:p-4 p-2 justify-start items-center gap-4 inline-flex lg:gap-4 gap-1">
+                    <RiskIcon
+                        riskFactor={riskLevels[0]?.tier}
+                        IconComponent={RiskIconBridge}
+                    />
+                    <RiskIcon
+                        riskFactor={riskLevels[1]?.tier}
+                        IconComponent={RiskIconDA}
+                    />
+                    <RiskIcon
+                        riskFactor={riskLevels[2]?.tier}
+                        IconComponent={RiskIconOperators}
+                    />
+                    <RiskIcon
+                        riskFactor={riskLevels[3]?.tier}
+                        IconComponent={RiskIconSettlement}
+                    />
+                </div>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-auto mx-auto max-w-[500px]">
+                <RiskSnapshot layer={layer} />
+            </HoverCardContent>
+        </HoverCard>
     );
 };
 
