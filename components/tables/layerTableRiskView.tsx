acdf041ef20c9e07d35f0796594b43225e090efb
Mykyta Sydorenko <mykytasydorenko0@gmail.com>
2024-10-10 12:17:37 +0300

    removed useless suffixes

--- components/tables/layerTableRiskView.tsx
+++ components/tables/layerTableRiskView.tsx
@@ -1,4 +1,4 @@
-import { allLayers } from "@/i18n/en/layer_index_en";
+import { allLayers } from "@/i18n/en/layer_index";
 import LayerTableRisks from "@/components/layer/layerTableRisks";
 import tableStyles from "../styles/tableStyles";
 
--- helpers/locale.helpers.ts
+++ helpers/locale.helpers.ts
@@ -1,24 +1,24 @@
 import { getUserLocale } from "@/services/locale";
 
-type LayersType = typeof import("@/i18n/en/layer_index_en");
+type LayersType = typeof import("@/i18n/en/layer_index");
 type ChartDataType =
     typeof import("@/messages/en/layers/bob_total_supply.json");
-type InfrastructureType = typeof import("@/i18n/en/infrastructure_index_en");
-type OpcodeTypes = typeof import("@/i18n/en/opcode_index_en");
+type InfrastructureType = typeof import("@/i18n/en/infrastructure_index");
+type OpcodeTypes = typeof import("@/i18n/en/opcode_index");
 
 export async function getAllInfrastructure(): Promise<InfrastructureType> {
     const locale = await getUserLocale();
-    return import(`@/i18n/${locale}/infrastructure_index_${locale}`);
+    return import(`@/i18n/${locale}/infrastructure_index`);
 }
 
 export async function getAllOpcodesWithSlug(): Promise<OpcodeTypes> {
     const locale = await getUserLocale();
-    return import(`@/i18n/${locale}/opcode_index_${locale}`);
+    return import(`@/i18n/${locale}/opcode_index`);
 }
 
 export async function getAllLayersWithSlugs(): Promise<LayersType> {
     const locale = await getUserLocale();
-    return import(`@/i18n/${locale}/layer_index_${locale}`);
+    return import(`@/i18n/${locale}/layer_index`);
 }
 
 export async function getChartData(): Promise<ChartDataType> {
