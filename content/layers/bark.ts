688fcdf95b3a32b2952adcd16fd89941574452ad
Janusz <82240624+januszgrze@users.noreply.github.com>
2024-10-30 12:05:55 -0400

    Create bark.ts

---
+++ content/layers/bark.ts
@@ -0,0 +1,82 @@
+import {
+    LayerProject,
+    Type,
+    LiveStatus,
+    RiskFactor,
+    EntityType,
+    Site,
+    RiskSection,
+    ContentSection,
+    RiskCategory,
+} from "../props";
+
+const bark: LayerProject = {
+    type: Type.Layer,
+    slug: "bark",
+    title: "Bark",
+    entityType: EntityType.Ark,
+    live: LiveStatus.Announced,
+    staking: false,
+    bridge: false,
+    underReview: true,
+    riskFactors: ["", "", "", ""],
+    btcLocked: 0,
+    nativeToken: "-",
+    feeToken: "-",
+    bitcoinOnly: false,
+    links: [
+        {
+            text: Site.Website,
+            url: "https://second.tech/",
+        },
+        {
+            text: Site.Twitter,
+            url: "https://x.com/2ndbtc",
+        },
+    ],
+    description: "Bark is an upcoming implementation of Ark.",
+    riskAnalysis: [
+        {
+            category: RiskCategory.UnilateralExits,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+        },
+        {
+            category: RiskCategory.DataAvailability,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+        },
+        {
+            category: RiskCategory.BlockProduction,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+        },
+        {
+            category: RiskCategory.StateValidation,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+        },
+    ],
+    sections: [
+        {
+            id: "selfsubmit",
+            title: "Process to self-submit information",
+            content: [
+                {
+                    content:
+                        "The Bitcoin Layers project prioritizes risk reviews on projects that are in production and accepting users' BTC deposits. Projects on testnet are welcome to submit information about their project. We do not publish risk assessments for projects that are not in production.\n\nHere are the [instructions](https://github.com/bitcoinlayers/bitcoinlayers/blob/main/SELFSUBMIT.md) on self-submitting a project.",
+                },
+            ],
+        },
+    ],
+};
+
+export default bark;
