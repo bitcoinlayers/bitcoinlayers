ea0076f2e0cebad40fcf17409fbc38af614e510c
Red <bradley.t.sheehan@gmail.com>
2024-11-13 14:53:09 -0700

    bitlayer capitalization

--- content/layers/bitlayer.ts
+++ content/layers/bitlayer.ts
@@ -61,7 +61,7 @@ const bitlayer: LayerProject = {
             tier: RiskFactor.VeryHigh,
             title: "Users trust federated signers set to custody their BTC. Less than 5, individual signers have been publicly announced",
             content:
-                "Bitlayer's current BTC bridge is a federated two-way peg with institutional signers. Bitlayer is working with multiple MPC custody platforms.\n\nUsers do not custody bitcoin assets backing tokens on BitLayer.\n\nNote that we are unable to verify the participants in this model - [Source](https://docs.bitlayer.org/docs/Learn/BitlayerNetwork/Bridges)",
+                "Bitlayer's current BTC bridge is a federated two-way peg with institutional signers. Bitlayer is working with multiple MPC custody platforms.\n\nUsers do not custody bitcoin assets backing tokens on Bitlayer.\n\nNote that we are unable to verify the participants in this model - [Source](https://docs.bitlayer.org/docs/Learn/BitlayerNetwork/Bridges)",
         },
         {
             category: RiskCategory.DataAvailability,
@@ -77,15 +77,15 @@ const bitlayer: LayerProject = {
             tier: RiskFactor.UnderReview,
             title: "Bitlayer is operated by a permissioned validator set",
             content:
-                "Bitlayer blocks are currently produced by a permissioned validator set. There are currently 21 validators participating in Bitlayer consensus. Users cannot bypass this operator set if they are censored by the validator set.\n\n🔬We are currently reviewing the identities of BitLayer's validators.",
+                "Bitlayer blocks are currently produced by a permissioned validator set. There are currently 21 validators participating in Bitlayer consensus. Users cannot bypass this operator set if they are censored by the validator set.\n\n🔬We are currently reviewing the identities of Bitlayer's validators.",
         },
         {
             category: RiskCategory.FinalityGuarantees,
             score: 0,
             tier: RiskFactor.High,
-            title: "BitLayer validators finalize blocks",
+            title: "Bitlayer validators finalize blocks",
             content:
-                "There is no onchain enforcement of Bitlayer state transitions on bitcoin. BitLayer's state transitions are finalized by a permissioned validator set. Validators sign off on proposed blocks and finalize them in the chain.",
+                "There is no onchain enforcement of Bitlayer state transitions on bitcoin. Bitlayer's state transitions are finalized by a permissioned validator set. Validators sign off on proposed blocks and finalize them in the chain.",
         },
     ],
     sections: [
