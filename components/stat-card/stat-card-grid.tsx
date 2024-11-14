a0f8c79edd7dffc95b8bc35d91528dd337a9e57b
Red <bradley.t.sheehan@gmail.com>
2024-11-13 20:25:24 -0700

    make stat cards more robust

--- components/stat-card/stat-card-grid.tsx
+++ components/stat-card/stat-card-grid.tsx
@@ -17,7 +17,7 @@ export default function StatCardGrid() {
         tvlTitle: "Total BTC locked",
         txTitle: "Total transactions",
         feeTitle: "Avg transaction fee",
-        addrTitle: "Active addressess",
+        addrTitle: "Active addresses",
     };
 
     const content = (() => {
@@ -27,10 +27,25 @@ export default function StatCardGrid() {
                     ...sharedTitles,
                     tvlSubtitle: "in wrapped BTC tokens",
                     txSubtitle: "using wrapped BTC tokens",
-                    feeSubtitle: "paid for wrapped BTC tx's",
+                    feeSubtitle: "per wrapped BTC tx",
                     addrSubtitle: "holding wrapped BTC tokens",
                 };
-            case "layers":
+            case "staking":
+                return {
+                    ...sharedTitles,
+                    tvlSubtitle: "in BTC staking protocols",
+                    txSubtitle: "using BTC staking protocols",
+                    feeSubtitle: "per BTC staking tx",
+                    addrSubtitle: "staking BTC",
+                };
+            case "liquidstaking":
+                return {
+                    ...sharedTitles,
+                    tvlSubtitle: "in BTC LSTS",
+                    txSubtitle: "using BTC LSTs",
+                    feeSubtitle: "per BTC LST tx",
+                    addrSubtitle: "using BTC LSTs",
+                };
             default:
                 return {
                     ...sharedTitles,
@@ -42,40 +57,130 @@ export default function StatCardGrid() {
         }
     })();
 
+    const TvlStatCard = (() => {
+        switch (view) {
+            case "wrappers":
+                return (
+                    <StatCard
+                        title={content.tvlTitle}
+                        subtitle={content.tvlSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<BitcoinIcon className="h-4" />}
+                    />
+                );
+            case "staking":
+                return (
+                    <StatCard
+                        title={content.tvlTitle}
+                        subtitle={content.tvlSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<BitcoinIcon className="h-4" />}
+                    />
+                );
+            case "liquidstaking":
+                return (
+                    <StatCard
+                        title={content.tvlTitle}
+                        subtitle={content.tvlSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<BitcoinIcon className="h-4" />}
+                    />
+                );
+            default:
+                return <LayerTvlStatCard />;
+        }
+    })();
+
+    const TxStatCard = (() => {
+        switch (view) {
+            case "wrappers":
+            case "staking":
+            case "liquidstaking":
+                return (
+                    <StatCard
+                        title={content.txTitle}
+                        subtitle={content.txSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<ArrowLeftRightIcon className="h-4" />}
+                    />
+                );
+            default:
+                return (
+                    <StatCard
+                        title={content.txTitle}
+                        subtitle={content.txSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<ArrowLeftRightIcon className="h-4" />}
+                    />
+                );
+        }
+    })();
+
+    const FeeStatCard = (() => {
+        switch (view) {
+            case "wrappers":
+            case "staking":
+            case "liquidstaking":
+                return (
+                    <StatCard
+                        title={content.feeTitle}
+                        subtitle={content.feeSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<HandCoinsIcon className="h-4" />}
+                    />
+                );
+            default:
+                return (
+                    <StatCard
+                        title={content.feeTitle}
+                        subtitle={content.feeSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<HandCoinsIcon className="h-4" />}
+                    />
+                );
+        }
+    })();
+
+    const AddrStatCard = (() => {
+        switch (view) {
+            case "wrappers":
+            case "staking":
+            case "liquidstaking":
+                return (
+                    <StatCard
+                        title={content.addrTitle}
+                        subtitle={content.addrSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<NotebookTabsIcon className="h-4" />}
+                    />
+                );
+            default:
+                return (
+                    <StatCard
+                        title={content.addrTitle}
+                        subtitle={content.addrSubtitle}
+                        isComingSoon
+                        change={0}
+                        symbol={<NotebookTabsIcon className="h-4" />}
+                    />
+                );
+        }
+    })();
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {!view || view === "layers" ? (
-                <LayerTvlStatCard />
-            ) : (
-                <StatCard
-                    title={content.tvlTitle}
-                    subtitle={content.tvlSubtitle}
-                    isComingSoon
-                    change={0}
-                    symbol={<BitcoinIcon className="h-4" />}
-                />
-            )}
-            <StatCard
-                title={content.txTitle}
-                subtitle={content.txSubtitle}
-                isComingSoon
-                change={0}
-                symbol={<ArrowLeftRightIcon className="h-4" />}
-            />
-            <StatCard
-                title={content.feeTitle}
-                subtitle={content.feeSubtitle}
-                isComingSoon
-                change={0}
-                symbol={<HandCoinsIcon className="h-4" />}
-            />
-            <StatCard
-                title={content.addrTitle}
-                subtitle={content.addrSubtitle}
-                isComingSoon
-                change={0}
-                symbol={<NotebookTabsIcon className="h-4" />}
-            />
+            {TvlStatCard}
+            {TxStatCard}
+            {FeeStatCard}
+            {AddrStatCard}
         </div>
     );
 }
