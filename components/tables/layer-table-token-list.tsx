7227b48752e4b31390edcc7d81b16dd16629fc2b
Red <bradley.t.sheehan@gmail.com>
2024-12-17 19:19:21 -0700

    fix null returns

--- components/tables/layer-table-token-list.tsx
+++ components/tables/layer-table-token-list.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { MappingRanked } from "@/hooks/use-get-mappings";
 
@@ -8,28 +8,34 @@ interface TokenListProps {
 
 const TokenList: React.FC<TokenListProps> = ({ tokens }) => {
     if (!tokens || tokens.length === 0) {
-        return <div>.....</div>;
+        return null;
     }
 
     const topTokens = tokens.slice(0, 3);
 
     return (
         <ul className="flex flex-wrap gap-2">
-            {topTokens.map((token) => (
-                <li key={token.token_slug} className="flex items-center gap-2">
-                    <Image
-                        src={`/logos/${token.token_slug}.png`}
-                        alt="" //{`${token.token_slug} icon`}
-                        width={20}
-                        height={20}
-                        // onError={(e) => {
-                        //     (e.target as HTMLImageElement).src =
-                        //         "/icons/btc.svg";
-                        // }}
-                    />
-                    {/* <span>{token.token_slug}</span> */}
-                </li>
-            ))}
+            {topTokens.map((token) => {
+                const [imageSrc, setImageSrc] = useState(
+                    `/logos/${token.token_slug}.png`,
+                );
+                const fallbackSrc = "/logos/bitcoinlayers-logo.png";
+
+                return (
+                    <li
+                        key={token.token_slug}
+                        className="flex items-center gap-2"
+                    >
+                        <Image
+                            src={imageSrc}
+                            alt=""
+                            width={20}
+                            height={20}
+                            onError={() => setImageSrc(fallbackSrc)}
+                        />
+                    </li>
+                );
+            })}
         </ul>
     );
 };
