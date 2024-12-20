e2419c2832092e64a190a628a51c076776aab400
Janusz <82240624+januszgrze@users.noreply.github.com>
2024-12-19 20:52:43 -0400

    Update bitfinity.ts

--- content/layers/bitfinity.ts
+++ content/layers/bitfinity.ts
@@ -19,7 +19,7 @@ const bitfinity: LayerProject = {
     staking: false,
     liquidStaking: false,
     bridge: false,
-    underReview: true,
+    underReview: false,
     riskFactors: [
         RiskFactor.UnderReview,
         RiskFactor.UnderReview,
@@ -53,45 +53,56 @@ const bitfinity: LayerProject = {
         },
     ],
     description:
-        "Bitfinity is an EVM-based Proof-of-Stake sidechain. It is currently live on testnet.",
+        "Bitfinity is an EVM-based sidechain.",
     riskAnalysis: [
         {
             category: RiskCategory.BtcCustody,
             score: 0,
-            tier: "",
+            tier: RiskFactor.NotApplicable,
             title: "",
             content: "",
-            pegs: [],
+            pegs: [
+                {
+                    name: "ICP ckBTC",
+                    infrastructureSlug: "icp-ckbtc",
+                    score: 0,
+                    tier: RiskFactor.UnderReview,
+                    title: "Leverages ICP's ckBTC two-way peg with bitcoin. We are reviewing if there are any changes for the Bitfinity integration",
+                    content:
+                        "Bitfinity's two-wag peg leverages ICP's ckBTC two-way peg to bring BTC onto the sidechain. BTC backing ckBTC is secured by validators selected by ICP's governance mechanism. This implementation is under review.",
+                },
+            ],
         },
         {
             category: RiskCategory.DataAvailability,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.UnderReview,
+            title: "We are reviewing what satisfies Bitfinity's data availability requirement",
+            content: "We are reviewing the operators responsible for data availability and storage for the Bitfinity sidechain.",
         },
         {
-            category: RiskCategory.BlockProduction,
+            category: RiskCategory.NetworkOperators,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.UnderReview,
+            title: "We are reviewing Bitfinity's block production mechanism",
+            content: "We are reviewing the operators of the Bitfinity sidechain.",
         },
         {
-            category: RiskCategory.StateValidation,
+            category: RiskCategory.FinalityGuarantees,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.UnderReview,
+            title: "We are reviewing Bitfinity's finality guarantees",
+            content: "We are reviewing how transactions are finalized on Bitfinity.",
         },
     ],
     sections: [
         {
             id: "underreview",
-            title: "Under review.",
+            title: "Further sections under review",
             content: [
                 {
-                    content: "This project is under review.",
+                    content:
+                        "Aspects related to bitcoin security, relevant technologies, and some two-way pegs have not been reviewed.\n\nThey will be reviewed by our team soon.",
                 },
             ],
         },
