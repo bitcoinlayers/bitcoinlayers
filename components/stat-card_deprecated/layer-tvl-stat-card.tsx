84702db070fa34c04d1ca00ee05e2ff210fb23a0
Red <bradley.t.sheehan@gmail.com>
2025-01-04 16:32:55 -0700

    WIP deprecate stat cards
    rm percentchange api endpoint

---
+++ components/stat-card_deprecated/layer-tvl-stat-card.tsx
@@ -0,0 +1,31 @@
+// import useGetLayertvlPercentchange from "@/hooks/zdep_use-get-layertvl-percentchange";
+// import StatCard from "../stat-card";
+// import { BitcoinIcon } from "lucide-react";
+
+// export default function LayerTvlStatCard() {
+//     const { data } = useGetLayertvlPercentchange();
+
+//     const bitcoinLayerData = data?.find(
+//         (item) => item.layer_name === "Bitcoin Layers Total",
+//     );
+
+//     const totalTVL = bitcoinLayerData?.amount_today || 0;
+//     const percentChangeMoM = bitcoinLayerData?.percent_change_mom || 0;
+
+//     const formattedTVL = new Intl.NumberFormat("en-US", {
+//         minimumFractionDigits: 2,
+//         maximumFractionDigits: 2,
+//     }).format(totalTVL);
+
+//     const formattedMoM = Math.round(percentChangeMoM * 100) / 100;
+
+//     return (
+//         <StatCard
+//             title="Total BTC locked"
+//             subtitle="in bitcoin layers"
+//             value={formattedTVL}
+//             change={formattedMoM}
+//             symbol={<BitcoinIcon className="h-4" />}
+//         />
+//     );
+// }
