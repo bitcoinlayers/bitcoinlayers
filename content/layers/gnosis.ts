8b0fbb9000a20a47422aa910346275b446e850cb
Red <bradley.t.sheehan@gmail.com>
2025-01-09 12:31:06 -0700

    empty layer pages for networks with data added

---
+++ content/layers/gnosis.ts
@@ -0,0 +1,100 @@
+import {
+    LayerProject,
+    Type,
+    LiveStatus,
+    RiskFactor,
+    EntityType,
+    Site,
+    RiskSection,
+    ContentSection,
+    RiskCategory,
+} from "../props";
+
+const gnosis: LayerProject = {
+    type: Type.Layer,
+    slug: "gnosis",
+    title: "Gnosis",
+    entityType: EntityType.AltL1,
+    live: LiveStatus.Mainnet,
+    staking: false,
+    liquidStaking: false,
+    bridge: false,
+    underReview: false,
+    riskFactors: [
+        RiskFactor.High,
+        RiskFactor.High,
+        RiskFactor.High,
+        RiskFactor.High,
+    ],
+    btcLocked: 0,
+    nativeToken: "-",
+    feeToken: "[x]BTC",
+    bitcoinOnly: false,
+    links: [
+        {
+            text: Site.Website,
+            url: "website",
+        },
+        {
+            text: Site.Docs,
+            url: "docs",
+        },
+        {
+            text: Site.Explorer,
+            url: "explorer",
+        },
+        {
+            text: Site.GitHub,
+            url: "github",
+        },
+        {
+            text: Site.Twitter,
+            url: "socials",
+        },
+    ],
+    description: "",
+    riskAnalysis: [
+        {
+            category: RiskCategory.BtcCustody,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+            pegs: [],
+        },
+        {
+            category: RiskCategory.DataAvailability,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+        },
+        {
+            category: RiskCategory.BlockProduction,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+        },
+        {
+            category: RiskCategory.StateValidation,
+            score: 0,
+            tier: "",
+            title: "",
+            content: "",
+        },
+    ],
+    sections: [
+        {
+            id: "description",
+            title: "Description",
+            content: [
+                {
+                    content: "Under review.",
+                },
+            ],
+        },
+    ],
+};
+
+export default gnosis;
