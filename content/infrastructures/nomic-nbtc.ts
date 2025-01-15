f6c0cdb23055c181990270381dfef28da2d2bc33
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-01-15 12:28:10 -0400

    we were right lol

--- content/infrastructures/nomic-nbtc.ts
+++ content/infrastructures/nomic-nbtc.ts
@@ -44,7 +44,7 @@ const nomicnbtc: InfrastructureProject = {
             tier: "",
             title: "BTC backing nBTC managed by a group of 20 publicly known signers who participate as validators in the Nomic blockchain",
             content:
-                "Users deposit BTC into a Reserve Wallet to receive nBTC on Nomic. The Reserve Wallet is a Bitcoin L1 multisig wallet managed by the Nomic signatory set. The Nomic signatory is made up of the top 20 Nomic validators measured by weighted stake.\n\nBecoming a signatory is permissionless and requires staking NOM tokens. Disbursing funds from the reserve wallet requires a threshold of signatories to sign, weighted by voting power through NOM tokens.\n\nWe are reviewing the signing threshold for this wallet.",
+                "Users deposit BTC into a Reserve Wallet to receive nBTC on Nomic. The Reserve Wallet is a Bitcoin L1 multisig wallet managed by the Nomic signatory set. The Nomic signatory is made up of the top 20 Nomic validators measured by weighted stake.\n\nBecoming a signatory requires staking NOM tokens. Disbursing funds from the reserve wallet requires a 90% threshold, weighted by voting power through NOM tokens.",
         },
     ],
 };
