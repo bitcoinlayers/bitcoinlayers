3e5d927744973afa85c4e62a1af48fc67a6467f8
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-01-15 22:02:13 -0400

    Update corn.ts
    Incorrect classification, claims to be an ethereum L2

--- content/layers/corn.ts
+++ content/layers/corn.ts
@@ -16,7 +16,7 @@ const corn: LayerProject = {
     slug: "corn",
     title: "Corn",
     entityType: EntityType.AltRollup,
-    entityCategory: EntityCategory.Sidesystem,
+    entityCategory: EntityCategory.Alt,
     live: LiveStatus.Mainnet,
     staking: false,
     liquidStaking: false,
