cd07a4a1b7e65bc422235a769abd5d18a7011699
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-01-30 10:11:38 -0400

    taiko snippets

--- content/layers/taiko.ts
+++ content/layers/taiko.ts
@@ -1,3 +1,4 @@
+import Risk from "@/components/layer/layerTableItemRisk";
 import {
     LayerProject,
     Type,
@@ -11,6 +12,10 @@ import {
     ContentSection,
     RiskCategory,
     TokenSnippet,
+    ReviewSnippet,
+    UseCaseSnippet,
+    TechnologySnippet,
+    BitcoinSecuritySnippet,
 } from "../props";
 
 const taiko: LayerProject = {
@@ -26,7 +31,7 @@ const taiko: LayerProject = {
     underReview: false,
     riskFactors: [
         RiskFactor.UnderReview,
-        RiskFactor.UnderReview,
+        RiskFactor.Medium,
         RiskFactor.UnderReview,
         RiskFactor.UnderReview,
     ],
@@ -65,37 +70,79 @@ const taiko: LayerProject = {
             tier: "",
             title: "",
             content: "",
-            pegs: [],
+            pegs: [
+                {
+                    name: "Solv SolvBTC",
+                    infrastructureSlug: "solv-solvbtc",
+                    score: 0,
+                    tier: RiskFactor.VeryHigh,
+                    title: TokenSnippet.CustodianPeg,
+                    content: TokenSnippet.SolvBTC,
+                },
+            ],
         },
         {
             category: RiskCategory.DataAvailability,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.Medium,
+            title: "Taiko uses Ethereum for data availability",
+            content: ReviewSnippet.EthereumRollupDA,
         },
         {
             category: RiskCategory.BlockProduction,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.Medium,
+            title: "Ethereum validators are responsible for sequencing Taiko transactions",
+            content: `${ReviewSnippet.BasedSequencedAlt}\n\nEthereum validators are responsible for sequencing Taiko transactions. We are reviewing if users can also self-sequence their own transactions.`,
         },
         {
             category: RiskCategory.StateValidation,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.UnderReview,
+            title: ReviewSnippet.UnderReview,
+            content: "Taiko's state validation mechanism has a [multi-tiered proof system](https://l2beat.com/scaling/projects/taiko). We are reviewing this mechanism.",
         },
     ],
     sections: [
         {
-            id: "description",
-            title: "Description",
+            id: "bitcoinsecurity",
+            title: "Bitcoin Security",
             content: [
                 {
-                    content: "Under review.",
+                    title: "Taiko does not inherit any security from bitcoin",
+                    content: BitcoinSecuritySnippet.NoSecurity,
+                },
+                {
+                    title: "ETH token used to pay fees",
+                    content: BitcoinSecuritySnippet.AltTokenFees,
+                },
+                {
+                    title: "No MEV introduced to bitcoin",
+                    content: "Taiko does not introduce MEV to bitcoin. Since it is a based sequenced rollups, it is possible for Ethereum block producers to extract MEV. Blocks in Ethereum are primarily auctioned off to builders who construct blocks on behalf of a proposer in a given slot. The majority of blocks in Ethereum are built by 2-3 builders.",
+                },
+                {
+                    title: "Ethereum does not contribute to the security budget",
+                    content: BitcoinSecuritySnippet.NoSecurity,
+                },
+            ],
+        },
+        {
+            id: "technology",
+            title: "Technology",
+            content: [
+                {
+                    title: "Ethereum Virtual Machine",
+                    content: TechnologySnippet.EVM,
+                },
+            ],
+        },
+        {
+            id: "usecases",
+            title: "Use Cases",
+            content: [
+                {
+                    title: "Onchain applications",
+                    content: UseCaseSnippet.OnchainApps
                 },
             ],
         },
