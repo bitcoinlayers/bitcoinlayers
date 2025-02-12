7b4c019ab4aefc2a33f07f07627e5950ba7b5c91
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-02-12 08:34:36 -0400

    pump btc review
    initial pump btc review

--- content/infrastructures/pump-pumpbtc.ts
+++ content/infrastructures/pump-pumpbtc.ts
@@ -7,6 +7,7 @@ import {
     Notice,
     Site,
     AssessmentCategory,
+    BTCWrapperTransparency,
 } from "../props";
 
 const pump: InfrastructureProject = {
@@ -38,10 +39,10 @@ const pump: InfrastructureProject = {
             text: Site.Explorer,
             url: "https://www.blockchain.com/explorer/assets/pumpbtc",
         },
-        // {
-        //     text: Site.GitHub,
-        //     url: "https://pumpbtc.gitbook.io/pumpbtc/key-address",
-        // },
+        {
+            text: Site.GitHub,
+            url: "https://github.com/pumpbtc",
+        },
         {
             text: Site.Twitter,
             url: "https://x.com/Pumpbtcxyz",
@@ -51,28 +52,40 @@ const pump: InfrastructureProject = {
         "Pump offers a mechanism that enables users to deposit wrapped BTC into smart contracts on EVM-based chains. Users receive PumpBTC, a token representing staked BTC, on Babylon in exchange for depositing a wrapped BTC token",
     sections: [
         {
-            id: "smartcontracts",
-            title: "Smart contracts & audits",
+            id: "protocoltransparency",
+            title: "Protocol Transparency",
             content: [
                 {
-                    title: "Contracts have been audited",
-                    content:
-                        "The contracts for PumpBTC have been audited. The audit reports can be found below.\n\n⚠️ Audits of smart contracts do not mean exploits are not possible. Users should not deposit more funds than they’re willing to lose.\n\n[Blocksec audit](https://drive.google.com/file/d/1kUmOPM8J63JmzM20H1Cg38oAfPRBJwLy/view?pli=1)\n\n[Quantstamp](https://drive.google.com/file/d/1xWQlC4iZenwHrjNOsBdKyVkFbEaUnwD-/view)",
+                    title: "Proof-of-Reserves",
+                    content: "The project provides a [list of BTC addresses](https://dashboard.pumpbtc.xyz/) that it claims to be its reserves.",
                 },
                 {
-                    title: "Contracts are permissioned",
-                    content:
-                        "Both contracts associated with PumpBTC are permissioned.\n\nThe staking contract can be:\n\n- Paused entirely.\n\n- A cap can be set, limiting the amount of assets that can be staked.\n\n- Have specific fees be set for unstaking, including extremely high fees (up to 100%).\n\n- Paused for unstaking and only allow staking.\n\nFor a full look into the staking contract, you can read it [here](https://etherscan.io/address/0x9e9dc47335ed4dc80051170d8101a70f689c4365#code).",
+                    title: "External operators disclosed",
+                    content: BTCWrapperTransparency.OperatorsDisclosedYes,
+                },
+                {
+                    title: "Redemptions enabled and documented",
+                    content: BTCWrapperTransparency.RedemptionsYes,
+                },
+                {
+                    title: "Contracts are open-source and verified",
+                    content: BTCWrapperTransparency.ContractsYes,
                 },
             ],
         },
         {
-            id: "selfsubmit",
-            title: "Further sections to be reviewed",
+            id: "additionalconsiderations",
+            title: "Additional Considerations",
             content: [
                 {
+                    title: "Key storage & signing",
                     content:
-                        "Aspects related to minting & burning, key management, transaction signing, and proof-of-reserves have not been reviewed. We are currently reviewing these sections.",
+                        "PumpBTC states that reserve wallets are managed by an MPC solution between Cobo, Coinover, and Cobo. EVM wallets managing derivative assets are signed by GnosisSafes.",
+                },
+                {
+                    title: "Cross-chain PumpBTC",
+                    content:
+                        "Users can also mint wrapped versions of PumpBTC to be moved to another chain. Crosschain minting and burning is handled by an implementation of LayerZero.",
                 },
             ],
         },
@@ -95,7 +108,31 @@ const pump: InfrastructureProject = {
             tier: "",
             title: "PumpBTC works with custodians to store bitcoin assets",
             content:
-                "PumpBTC works with custodial providers to store the BTC that matches PumpBTC deposits. When a user deposits a BTC derivative token into the PumpBTC contract, they are given PumpBTC in return.\n\nCobo and Coinover have been mentioned as operators participating in Pump.\n\nUsers trust Pump's claims in their documentation are being executed in practice.\n\n[Source](https://pumpbtc.gitbook.io/pumpbtc/custody-and-security/how-does-pumpbtc-ensure-safety)",
+                "PumpBTC works with custodial providers to swap PumpBTC deposits into native BTC for BTC staking. When a user deposits a BTC derivative token (e.g. wBTC) into the PumpBTC contract, they are given PumpBTC in return. The staking contract is operated by a [⅔ GnosisSafe](https://etherscan.io/address/0xAC364d14020f1da0044699691a91f06ca6131Fe3).\n\nCobo and Coinover have been mentioned as operators participating in Pump.\n\n[Source](https://pumpbtc.gitbook.io/pumpbtc/custody-and-security/how-does-pumpbtc-ensure-safety)",
+        },
+        {
+            category: AssessmentCategory.SupplyIssuance,
+            score: 0,
+            tier: "",
+            title: "Minting of PumpBTC is managed by a smart contract",
+            content:
+                "PumpBTC is minted through a smart contract. Users deposit a wrapped version of BTC on an EVM chain into the contract and then receive PumpBTC.",
+        },
+        {
+            category: AssessmentCategory.CensorshipResistance,
+            score: 0,
+            tier: "",
+            title: "No blacklist function currently implemented",
+            content:
+                "Implementations of the token do not have a blacklist or pause function.",
+        },
+        {
+            category: AssessmentCategory.Governance,
+            score: 0,
+            tier: "",
+            title: "A centralized party can assign roles",
+            content:
+                "A [⅔ GnosisSafe](https://etherscan.io/address/0x77A0545Dc1Dc6bAee8d9c1d436c6688a75Ae5777) is responsible for assigning roles for various PumpBTC implementations. The token contract is not upgradeable.",
         },
     ],
 };
