c70b2651cd6d6e572d618f7e3340d1772829e4c4
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-02-14 08:29:31 -0400

    Update unirouter-ubtc.ts

--- content/infrastructures/unirouter-ubtc.ts
+++ content/infrastructures/unirouter-ubtc.ts
@@ -7,6 +7,7 @@ import {
     Notice,
     Site,
     AssessmentCategory,
+    BTCWrapperTransparency,
 } from "../props";
 
 const unirouter: InfrastructureProject = {
@@ -35,6 +36,10 @@ const unirouter: InfrastructureProject = {
             url: "https://docs.unirouter.io",
         },
         {
+            text: Site.GitHub,
+            url: "https://github.com/unirouter",
+        },
+        {
             text: Site.Explorer,
             url: "https://explorer.bsquared.network/address/0x796e4D53067FF374B89b2Ac101ce0c1f72ccaAc2",
         },
@@ -47,12 +52,25 @@ const unirouter: InfrastructureProject = {
         "uBTC is a liquid staking token that is operated by the UniRouter team. It is live on Bsquared Network.",
     sections: [
         {
-            id: "selfsubmit",
-            title: "Further sections to be reviewed",
+            id: "protocoltransparency",
+            title: "Protocol Transparency",
             content: [
                 {
+                    title: "Proof-of-Reserves",
                     content:
-                        "Aspects related to minting & burning, key management, transaction signing, and proof-of-reserves have not been reviewed. We are currently reviewing these sections.",
+                        "The project has not published a Proof-of-Reserves. Users trust that the custodians holding native BTC backing the derivative asset to have ample reserves.",
+                },
+                {
+                    title: "External operators disclosed",
+                    content: BTCWrapperTransparency.OperatorsDisclosedNo,
+                },
+                {
+                    title: "Redemptions enabled and documented",
+                    content: BTCWrapperTransparency.RedemptionsYesNoDocs,
+                },
+                {
+                    title: "Contracts are open-source and verified",
+                    content: BTCWrapperTransparency.ContractsNo,
                 },
             ],
         },
@@ -64,7 +82,31 @@ const unirouter: InfrastructureProject = {
             tier: "",
             title: "Users trust UniRouter with their custody practices. UniRouter has not disclosed who manages the BTC backing uBTC.",
             content:
-                "Users trust that the UniRouter team has set up secure custody practices and has BTC reserves backing uniBTC. UniRouter has not disclosed who secures the BTC backing uBTC.",
+                "Users trust that the UniRouter team has set up secure custody practices and has BTC reserves backing uBTC. UniRouter has not disclosed who secures the BTC backing uBTC.",
+        },
+        {
+            category: AssessmentCategory.SupplyIssuance,
+            score: 0,
+            tier: "",
+            title: "Contracts are not verified",
+            content:
+                "There’s no documentation on how the project manages supply issuance.",
+        },
+        {
+            category: AssessmentCategory.CensorshipResistance,
+            score: 0,
+            tier: "",
+            title: "Contracts are not verified",
+            content:
+                "We cannot currently verify if the token has a blacklist or pause function.",
+        },
+        {
+            category: AssessmentCategory.Governance,
+            score: 0,
+            tier: "",
+            title: "Contracts are not verified",
+            content:
+                "We cannot currently verify who controls relevant contracts and upgrade mechanisms.",
         },
     ],
 };
