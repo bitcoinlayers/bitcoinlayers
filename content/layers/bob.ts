3f135cd319db4aac4e79aff270b6b3a92916da87
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-02-24 19:11:14 -0400

    Update bob.ts

--- content/layers/bob.ts
+++ content/layers/bob.ts
@@ -68,7 +68,7 @@ const bob: LayerProject = {
                     tier: RiskFactor.VeryHigh,
                     title: "BTC users trust that tBTC will remain backed on Ethereum, and that the BOB bridge will not steal their funds",
                     content:
-                        "tBTC is minted on BOB via its official Ethereum bridge. tBTC on Ethereum is backed by BTC managed by the Threshold Network.\n\nUsers trust that the bridge operators will not steal their BTC, the BOB proposer to not publish a malicious, unchallenged state transition, or the admin controlling the bridge to create a malicious smart contract upgrade.\n\nIn the event of a malicious smart contract upgrade, there is no exit window for BOB users. This means that the admin behind the bridge can steal all funds in the official bridge.\n\nFor more information on the Threshold Network & tBTC, [click here.](https://www.bitcoinlayers.org/infrastructure/tbtc).",
+                        "tBTC is minted on BOB via its official Ethereum bridge. tBTC on Ethereum is backed by BTC managed by the Threshold Network.\n\nUsers trust that the bridge operators will not steal their BTC, the BOB proposer to not publish a malicious, unchallenged state transition, or the admin controlling the bridge to create a malicious smart contract upgrade.\n\nIn the event of a malicious smart contract upgrade, there is no exit window for BOB users. This means that the admin behind the bridge can steal all funds in the official bridge.",
                 },
                 {
                     name: "BitGo wBTC",
