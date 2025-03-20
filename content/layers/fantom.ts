8a98bedb086a2179fda2ff925c657e4d3cfe91d6
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-03-20 13:12:54 -0400

    Update fantom.ts

--- content/layers/fantom.ts
+++ content/layers/fantom.ts
@@ -11,6 +11,8 @@ import {
     ContentSection,
     RiskCategory,
     TokenSnippet,
+    ReviewSnippet,
+    BitcoinSecuritySnippet,
 } from "../props";
 
 const fantom: LayerProject = {
@@ -25,10 +27,10 @@ const fantom: LayerProject = {
     bridge: false,
     underReview: false,
     riskFactors: [
-        RiskFactor.High,
-        RiskFactor.High,
-        RiskFactor.High,
-        RiskFactor.High,
+        RiskFactor.VeryHigh,
+        RiskFactor.VeryHigh,
+        RiskFactor.VeryHigh,
+        RiskFactor.VeryHigh,
     ],
     btcLocked: 0,
     nativeToken: "-",
@@ -46,7 +48,7 @@ const fantom: LayerProject = {
         },
         {
             text: Site.Explorer,
-            url: "https://sonicscan.org/",
+            url: "https://explorer.fantom.network/",
         },
         {
             text: Site.GitHub,
@@ -65,37 +67,70 @@ const fantom: LayerProject = {
             tier: "",
             title: "",
             content: "",
-            pegs: [],
+            pegs: [
+                {
+                    name: "BitGo wBTC",
+                    infrastructureSlug: "bitgo-wbtc",
+                    score: 0,
+                    tier: RiskFactor.VeryHigh,
+                    title: TokenSnippet.CustodianPeg,
+                    content: `${TokenSnippet.BitGowBTC}\n\n${TokenSnippet.smartcontractreview},`
+                },
+            ],
         },
         {
             category: RiskCategory.DataAvailability,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.VeryHigh,
+            title: "Data is stored and made available by Fantom full nodes. ",
+            content:
+                "The data for Fantom's state is made available by its full nodes. We are reviewing if operating a node is permissionless.",
         },
         {
-            category: RiskCategory.BlockProduction,
+            category: RiskCategory.NetworkOperators,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.VeryHigh,
+            title: "Fantom is operated by a federated validator set",
+            content: ReviewSnippet.AltL1Operators
         },
         {
-            category: RiskCategory.StateValidation,
+            category: RiskCategory.FinalityGuarantees,
             score: 0,
-            tier: "",
-            title: "",
-            content: "",
+            tier: RiskFactor.VeryHigh,
+            title: "We are currently reviewing Fantom's finality guarantees",
+            content: "Finality guarantees are provided via an alternative proof-of-stake network. We are reviewing Fantom's consensus mechanism.",
         },
     ],
     sections: [
         {
-            id: "description",
-            title: "Description",
+            id: "bitcoinsecurity",
+            title: "Bitcoin Security",
             content: [
                 {
-                    content: "Under review.",
+                    title: "Fantom does not inherit any security from Bitcoin",
+                    content: BitcoinSecuritySnippet.NoSecurity,
+                },
+                {
+                    title: "FTM token used to pay fees",
+                    content: BitcoinSecuritySnippet.AltTokenFees,
+                },
+                {
+                    title: "No MEV introduced to Bitcoin",
+                    content: BitcoinSecuritySnippet.AltNetworkMEV,
+                },
+                {
+                    title: "Fantom does not contribute to the security budget",
+                    content: BitcoinSecuritySnippet.NoSecurityBudget,
+                },
+            ],
+        },
+        {
+            id: "deprecated",
+            title: "Network is no longer being developed",
+            content: [
+                {
+                    content:
+                        "The Fantom network is undergoing a migration to the Sonic network. FTM token holders are able to swap the FTM for S. While the network is still operating, we consider it being deprecated. BTC users should work directly with their bridge provider to exit the Fantom network. Due to this migration, we have given Fantom a custom Very High trust assumption score.",
                 },
             ],
         },
