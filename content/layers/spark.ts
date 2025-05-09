f13d6556dd7d31b3777716dc4781de0a2deed3bf
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-05-09 10:41:39 -0400

    spark first version

--- content/layers/spark.ts
+++ content/layers/spark.ts
@@ -12,6 +12,9 @@ import {
     RiskCategory,
     TokenSnippet,
     ReviewSnippet,
+    UseCaseSnippet,
+    BitcoinSecuritySnippet,
+    TechnologySnippet,
 } from "../props";
 
 const spark: LayerProject = {
@@ -64,7 +67,7 @@ const spark: LayerProject = {
                     infrastructureSlug: "spark-btc",
                     score: 0,
                     tier: RiskFactor.Low,
-                    title: "Users collaboratively custody BTC backing offchain UTXOs with a federation",
+                    title: "Users collaboratively custody funds with the statechain entitiy",
                     content: TokenSnippet.SparkBTC,
                 },
             ],
@@ -73,8 +76,8 @@ const spark: LayerProject = {
             category: RiskCategory.DataAvailability,
             score: 0,
             tier: RiskFactor.Low,
-            title: "This section is under review",
-            content: "This section is under review.",
+            title: "Transfer history data is held client-side",
+            content: "Transaction data is self-hosted. The statechain entity signs individual transactions and the transfer history gets passed on between clients.",
         },
         {
             category: RiskCategory.BlockProduction,
@@ -92,7 +95,67 @@ const spark: LayerProject = {
         },
     ],
     sections: [
-
+        {
+            id: "bitcoinsecurity",
+            title: "Bitcoin Security",
+            content: [
+                {
+                    title: "Settlement is finalized by Bitcoin consensus",
+                    content: BitcoinSecuritySnippet.OffchainUTXO,
+                },
+                {
+                    title: "The protocol does not enable MEV on Bitcoin",
+                    content: BitcoinSecuritySnippet.OffchainUTXOMEV,
+                },
+                {
+                    title: "No alternative token needed for network security",
+                    content: BitcoinSecuritySnippet.OffchainUTXONoToken,
+                },
+                {
+                    title: "Unilateral exits contribute to the security budget",
+                    content: BitcoinSecuritySnippet.StatechainSecurityBudget,
+                },
+            ],
+        },
+        {
+            id: "Technology",
+            title: "Technology",
+            content: [
+                {
+                    title: "Statechains",
+                    content: TechnologySnippet.Statechain,
+                },
+                {
+                    title: "FROST",
+                    content: `${TechnologySnippet.FROST}\n\nSpark leverages FROST for its statechain entity. A threshold of statechain operators are needed to create a valid signature for signing events.`,
+                },
+            ]
+        },
+        {
+            id: "usecases",
+            title: "Use Cases",
+            content: [
+                {
+                    title: "Offchain UTXO transfers",
+                    content: UseCaseSnippet.OffchainUTXOTransfers,
+                },
+                {
+                    title: "Tokenized applications",
+                    content: UseCaseSnippet.UTXOTokenizedApplications,
+                },
+            ],
+        },
+        {
+            id: "knowledgeBits",
+            title: "Knowledge Bits",
+            content: [
+                {
+                    title: "Learn more about statechains below",
+                    content:
+                        "Statechains Whitepaper by Ruben Somsen ([GitHub, Oct 2018](https://github.com/RubenSomsen/rubensomsen.github.io/blob/master/img/statechains.pdf)) \n Statechains: Non-custodial Off-chain Bitcoin Transfer by Ruben Somsen ([Medium, Jun 2019](https://medium.com/@RubenSomsen/statechains-non-custodial-off-chain-bitcoin-transfer-1ae4845a4a39#:~:text=Statechains%20are%20a%20layer%20two,with%20scaling%20and%20save%20fees.))",
+                },
+            ],
+        },
     ],
 };
 
