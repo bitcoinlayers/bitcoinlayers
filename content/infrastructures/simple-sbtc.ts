a5cd78e07051b2b16ef68ded7cd8fafeed84705c
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-05-23 06:15:55 -0400

    some updates

--- content/infrastructures/simple-sbtc.ts
+++ content/infrastructures/simple-sbtc.ts
@@ -6,6 +6,7 @@ import {
     EntityType,
     Notice,
     Site,
+    AssessmentCategory
 } from "../props";
 
 const simplesbtc: InfrastructureProject = {
@@ -27,17 +28,27 @@ const simplesbtc: InfrastructureProject = {
     links: [],
     description: "Under review.",
     sections: [
-        {
-            id: "selfsubmit",
-            title: "Process to self-submit information",
-            content: [
                 {
-                    content:
-                        "The Bitcoin Layers project prioritizes risk reviews on projects that are in production and accepting users' BTC deposits. Projects on testnet are welcome to submit information about their project. We do not publish risk assessments for projects that are not in production.\n\nHere are the [instructions](https://github.com/bitcoinlayers/bitcoinlayers/blob/main/SELFSUBMIT.md) on self-submitting a project.",
+                    id: "selfsubmit",
+                    title: "Further sections to be reviewed",
+                    content: [
+                        {
+                            content:
+                                "Aspects related to minting & burning, key management, transaction signing, and proof-of-reserves have not been reviewed. We are currently reviewing these sections.",
+                        },
+                    ],
                 },
             ],
-        },
-    ],
+            assessment: [
+                {
+                    category: AssessmentCategory.AssetCustody,
+                    score: 0,
+                    tier: "",
+                    title: "A 3/5 multisig secures the BTC backing Simple sBTC",
+                    content:
+                        "BTC backing Simple sBTC is secured by a [3/5 multisig](https://mempool.space/address/bc1ps0qa22q30rrp4584gz4teqkchn76wakzaq6mlhsv6sg36e0fl83sss2vxa). Information on who the signers are for this multisig and their signing mechanisms is unavailable.",
+                },
+            ],
 };
 
 export default simplesbtc;
