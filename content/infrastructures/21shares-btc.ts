ca5d461678a39f79d4c707bc2aa402f61a2a33b7
Janusz <82240624+januszgrze@users.noreply.github.com>
2025-05-24 07:58:48 -0400

    Update 21shares-btc.ts

--- content/infrastructures/21shares-btc.ts
+++ content/infrastructures/21shares-btc.ts
@@ -11,7 +11,7 @@ import {
 
 const twentyonesharesbtc: InfrastructureProject = {
     type: Type.Infrastructure,
-    slug: "1shares-21btc",
+    slug: "21shares-21btc",
     title: "21 Shares BTC",
     entityType: EntityType.ReserveAsset,
     live: LiveStatus.Mainnet,
